cdad4e809055e70115b3a4af6ff23fe8b0963fe9
hamgamsa <hamgamsa@gmail.com>
2019-04-09 17:20:56 +0900

    1

---
+++ spring/src/DaoFactory.java
@@ -0,0 +1,25 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+
+	@Bean
+	public UserDao userDao(){
+		ConnectionMaker connectionMaker = connectionMaker();
+		
+		UserDao userDao = new UserDao(connectionMaker);
+		
+		return userDao;
+		
+	}
+
+	@Bean
+	public ConnectionMaker connectionMaker(){
+		
+		return new DConnectionMaker();
+	}
+	
+	
+	
+}
