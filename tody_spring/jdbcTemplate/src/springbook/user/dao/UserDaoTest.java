397f7eb3abb9017545d6a733fc3b89e144f86eb5
hamgamsa <hamgamsa@gmail.com>
2019-04-11 18:07:02 +0900

    1

--- tody_spring/jdbcTemplate/src/springbook/user/dao/UserDaoTest.java
+++ tody_spring/jdbcTemplate/src/springbook/user/dao/UserDaoTest.java
@@ -5,13 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -27,17 +33,21 @@ public class UserDaoTest {
 	@Autowired
 	private ApplicationContext context;
 
+
+	User user1;
+	User user2;
+	User user3;
+
 	@Before
 	public void setup(){
-
+		this.user1 = new User("gyumee", "박성철", "springno1",Level.BASIC, 1, 0);
+		this.user2 = new User("leegw700", "이길원", "springno2",Level.SILVER, 55, 10);
+		this.user3 = new User("bumjin", "박범진", "springno3",Level.GOLD, 100, 40);
 	}
 
 
 	@Test
 	public void addAndGet() throws SQLException {
-
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -48,23 +58,18 @@ public class UserDaoTest {
 		
 		User userget1 = dao.get(user1.getId());
 
-		assertThat(userget1.getName(), is(user1.getName()));
-		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		checkSameUser(userget1, user1);
 
 		User userget2 = dao.get(user2.getId());
 
-		assertThat(userget2.getName(), is(user2.getName()));
-		assertThat(userget2.getPassword(), is(user2.getPassword()));
+		checkSameUser(userget2, user2);
+
 
 	}
 
 
 	@Test
 	public void count() throws SQLException {
-
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
-		User user3 = new User("bumjin", "박범진", "springno3");
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -92,7 +97,75 @@ public class UserDaoTest {
 
 	}
 
+	@Test
+	public void getAll() throws SQLException {
 
+		dao.deleteAll();
+
+		List<User> users0 = dao.getAll();
+		assertThat(users0.size(), is(0));
+
+		dao.add(user1);
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+
+		dao.add(user2);
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));
+		checkSameUser(user2, users2.get(1));
+
+		dao.add(user3);
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user3, users3.get(0));
+		checkSameUser(user1, users3.get(1));
+		checkSameUser(user2, users3.get(2));
+
+
+	}
+
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
+		assertThat(user1.getLevel(), is(user2.getLevel()));
+		assertThat(user1.getLogin(), is(user2.getLogin()));
+		assertThat(user1.getRecommend(), is(user2.getRecommend()));
+
+	}
+
+	@Test(expected = DuplicateKeyException.class)
+	public void duplicateKey(){
+		dao.deleteAll();
+
+		dao.add(user1);
+		dao.add(user1);
+	}
+
+	@Test
+	public void update(){
+		dao.deleteAll();
+
+		dao.add(user1);
+		dao.add(user2);
+
+		user1.setName("민규");
+		user1.setPassword("springno6");
+		user1.setLevel(Level.GOLD);
+		user1.setLogin(1000);
+		user1.setRecommend(999);
+		dao.update(user1);
+
+		User user1update = dao.get(user1.getId());
+		checkSameUser(user1, user1update);
+
+		User user2update = dao.get(user2.getId());
+		checkSameUser(user2, user2update);
+
+
+	}
 
 
 
---
+++ tody_spring/jdbcTemplate/src/springbook/user/service/UserServiceTest.java
@@ -0,0 +1,92 @@
+package springbook.user.service;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.dao.Level;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
+public class UserServiceTest {
+
+    @Autowired
+    UserDao userDao;
+
+    @Autowired
+    UserService userService;
+
+    List<User> users;
+
+    @Before
+    public void setUp(){
+        users = Arrays.asList(
+                 new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0)
+                ,new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0)
+                ,new User("erwins", "신승환", "p3", Level.SILVER, 60, 29)
+                ,new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30)
+                ,new User("green", "오만규", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void bean(){
+        assertThat(this.userService, is(notNullValue()));
+    }
+
+    @Test
+    public void upgradeLevels(){
+        userDao.deleteAll();
+
+        for(User user : users){
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+
+    }
+
+    private void checkLevel(User user, Level expectedLevel){
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
+
+    }
+
+    @Test
+    public void add(){
+        userDao.deleteAll();
+
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead =userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+        assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+
+    }
+
+}
