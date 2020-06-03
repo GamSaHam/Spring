b7118bd099b2ded2808333a9949fa413f0e06a4b
gsham <gsham@12cm.co.kr>
2020-06-03 17:55:46 +0900

    1

---
+++ batch/batchexam/src/main/java/com/example/batchexam/job/JobConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.batchexam.job;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job myJob(){
+        return jobBuilderFactory.get("myJob")
+                .start(myStep1())
+                .build();
+    }
+
+    @Bean
+    public Step myStep1(){
+        return stepBuilderFactory.get("myStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+
+    }
+
+}
